bcadfac63d58e9250facd337f5f0f8d1ec3fd202
Jürgen Truu <jtruu@tktk.ee>
2022-05-19 19:35:14 +0300

    Week5

---
+++ DiceGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int cpuRandom = rnd.Next(1, 7);
+            int userRandom = rnd.Next(1, 7);
+            int cpuScore = 0;
+            int userScore = 0;
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viskas {userRandom}");
+
+                if (cpuRandom < userRandom)
+                {
+                    userScore++;
+                }
+                else
+                {
+                    cpuScore++;
+                }
+                
+                cpuRandom = rnd.Next(1, 7);
+
+                
+                userRandom = rnd.Next(1, 7);
+                i++;
+
+            }
+            if (userScore > cpuScore)
+            {
+                Console.WriteLine($"Kasutaja võitis mängu {userScore} punktiga.");
+            }
+            else
+            {
+                Console.WriteLine($"Arvuti võitis mängu {cpuScore} punktiga.");
+            }
+
+            if (userScore == cpuScore)
+            {
+                Console.WriteLine("Viik!");
+            }
+
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string UserName = Console.ReadLine();
+                
+                Console.WriteLine("Sisesta salasõna");
+                string UserPassword = Console.ReadLine();
+                
+                if (UserName == "admin" && UserPassword == "admin1234")
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+                else
+                {
+                    i = i + 1;
+                    Console.WriteLine($"Vale kasutajatunnus või salasõna. {3 - i} katset on jäänud");
+                }
+
+
+            }
+            
+
+        }
+    }
+}
